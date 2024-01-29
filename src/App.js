7c7c17e79653806bf57526ffc715069c7e9b78dc
abhishek <abhishekaglave@grampwoer.com>
2024-01-29 16:18:49 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,23 @@
 import './App.css';
-// import React, { useNavigate } from 'react';
-// import FinalExamApi from './examComponent/FinalExam_api.jsx';
+import React from 'react';
 import LoginPage from './loginComponent/LoginPage';
-import ProfilePage from './profileComponrnt/ProfilePage';
+import ProfilePage from './profileComponent/ProfilePage';
 import LogIn from './loginComponent/LogIn';
-import DrivingAssessment from './examComponent/DrivingAssessment.jsx'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Results from './examComponent/Results'
-// import Header from './Header.jsx';
+import DrivingAssessment from './examComponent/DrivingAssessment.jsx';
+import { Route, Routes } from 'react-router-dom';
+import Results from './examComponent/Results';
 
 function App() {
-
   return (
     <div className="App">
-
-
-      <div>
-      <LoginPage />
-      <LogIn />
-      <ProfilePage />
-      <DrivingAssessment/>
-      <Results/>
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/driving-assessment" element={<DrivingAssessment />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
     </div>
   );
 }
 export default App;
-// <FinalExamApi/>
-
-
-
-// App.js
-
-// import './App.css';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import LoginPage from './loginComponent/LoginPage';
-// import LogIn from './loginComponent/LogIn';
-// import ProfilePage from './profileComponent/ProfilePage';
-// import DrivingAssessment from './examComponent/DrivingAssessment';
-// import Results from './examComponent/Results';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Routes>
-//           <Route path="/" element={<LoginPage />} />
-//           <Route path="/login" element={<LogIn />} />
-//           <Route path="/profile" element={<ProfilePage />} />
-//           <Route path="/driving-assessment" element={<DrivingAssessment />} />
-//           <Route path="/results" element={<Results />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
